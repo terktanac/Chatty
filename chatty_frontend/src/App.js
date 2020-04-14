89c97998b8a9b94446a7a0a0d0039c55b6b9ed49
SPRYK <Seth214@hotmail.co>
2020-04-15 02:31:43 +0700

    add test program

--- chatty_frontend/src/App.js
+++ chatty_frontend/src/App.js
@@ -1,26 +1,240 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// database
+import firebase from "firebase";
 
+// material ui
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  DialogTitle,
+  Dialog,
+  Button,
+  Typography,
+  AppBar,
+  Toolbar
+} from "@material-ui/core";
+
+// custom chat
+import { GiftedChat } from "react-web-gifted-chat";
+
+const config = {
+  apiKey: "AIzaSyCFHAzrXFy6mMU3jkzbz-4TXvDTDdQyZak",
+  authDomain: "chat-d8714.firebaseapp.com",
+  databaseURL: "https://chat-d8714.firebaseio.com",
+  projectId: "chat-d8714",
+  storageBucket: "chat-d8714.appspot.com",
+  messagingSenderId: "279941056049",
+  appId: "1:279941056049:web:ed4998671258c1f88a7710",
+  measurementId: "G-YQH7E78NRG"
+};
+firebase.initializeApp(config);
+
+const styles = {
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    height: "100vh",
+  },
+  channelList: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+  },
+  chat: {
+    display: "flex",
+    flex: 3,
+    flexDirection: "column",
+    borderWidth: "1px",
+    borderColor: "#ccc",
+    borderRightStyle: "solid",
+    borderLeftStyle: "solid",
+  },
+  settings: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+  },
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      user: {},
+      isAuthenticated: false,
+    };
+  }
+
+  async signIn() {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    try {
+      await firebase.auth().signInWithPopup(googleProvider);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  signOut() {
+    firebase.auth().signOut();
+  }
+
+  loadMessages() {
+    const callback = (snap) => {
+      const message = snap.val();
+      message.id = snap.key;
+      const { messages } = this.state;
+      messages.push(message);
+      this.setState({ messages });
+    };
+    firebase
+      .database()
+      .ref("/messages/")
+      .limitToLast(12)
+      .on("child_added", callback);
+  }
+
+  onSend(messages) {
+    for (const message of messages) {
+      this.saveMessage(message);
+    }
+  }
+
+  saveMessage(message) {
+    return firebase
+      .database()
+      .ref("/messages/")
+      .push(message)
+      .catch(function (error) {
+        console.error("Error saving message to Database:", error);
+      });
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ isAuthenticated: true, user });
+        this.loadMessages();
+      } else {
+        this.setState({ isAuthenticated: false, user: {}, messages: [] });
+      }
+    });
+  }
+
+  renderPopup() {
+    return (
+      <Dialog open={!this.state.isAuthenticated}>
+        <DialogTitle id="simple-dialog-title">Sign in</DialogTitle>
+        <div>
+          <List>
+            <ListItem button onClick={() => this.signIn()}>
+              <ListItemAvatar>
+                <Avatar style={{ backgroundColor: "#eee" }}>
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                    height="30"
+                    alt="G"
+                  />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Sign in with Google" />
+            </ListItem>
+          </List>
+        </div>
+      </Dialog>
+    );
+  }
+
+  renderSignOutButton() {
+    if (this.state.isAuthenticated) {
+      return <Button onClick={() => this.signOut()}>Sign out</Button>;
+    }
+    return null;
+  }
+
+  renderChat() {
+    return (
+      <GiftedChat
+        user={this.chatUser}
+        messages={this.state.messages.slice().reverse()}
+        onSend={(messages) => this.onSend(messages)}
+      />
+    );
+  }
+
+  renderChannels() {
+    return (
+      <List>
+        <ListItem button>
+          <ListItemAvatar>
+            <Avatar>D</Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Default" />
+        </ListItem>
+      </List>
+    );
+  }
+
+  renderChatHeader() {
+    return (
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Default channel
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+  renderSettingsHeader() {
+    return (
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Settings
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+
+  renderChannelsHeader() {
+    return (
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Channels
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        {this.renderPopup()}
+        <div style={styles.channelList}>
+          {this.renderChannelsHeader()}
+          {this.renderChannels()}
+        </div>
+        <div style={styles.chat}>
+          {this.renderChatHeader()}
+          {this.renderChat()}
+        </div>
+        <div style={styles.settings}>
+          {this.renderSettingsHeader()}
+          {this.renderSignOutButton()}
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
