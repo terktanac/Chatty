a2b2312cc14975cab94d7a651cc3cfdf5c28d7c7
endifly <ducth_miw@hotmail.com>
2020-04-23 20:12:33 +0700

    init web socket at controller

--- chatty-nest/src/app.controller.ts
+++ chatty-nest/src/app.controller.ts
@@ -10,3 +10,22 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 4000 });
+
+//wait client data
+wss.on('connection', function connection(ws) { 
+  ws.on('message', function incoming(message) {
+   
+    console.log('received: %s', message);
+  });
+
+//close browser
+ws.on('close', function close() {
+  
+    console.log('disconnected');
+  });
+
+//send data
+ws.send('init message to client');
