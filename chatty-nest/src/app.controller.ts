3b57ea72094e29dfa34fb754a14b670229c46c64
endifly <ducth_miw@hotmail.com>
2020-04-29 16:21:34 +0700

    mockup html

--- chatty-nest/src/app.controller.ts
+++ chatty-nest/src/app.controller.ts
@@ -17,8 +17,19 @@ const wss = new WebSocket.Server({ port: 4000 });
 //wait client data
 wss.on('connection', function connection(ws) {
   ws.on('message',function(message) {
-
+    console.log(message)
+    //console.log(ws)
+    //console.log("----------------------------------------")
+    //console.log(wss)
+    
+    //storee
   })
   ws.send("hello")
 
+
+
+  ws.on('close', function () {
+    console.log("lost"+ws.name)
+  })
+  
 });
