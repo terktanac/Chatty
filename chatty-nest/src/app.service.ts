9f56fe5e814a4111558383df52edd1e9c7e5c901
GottaK <chan_tham@hotmail.com>
2020-05-02 01:17:04 +0700

    ip

--- chatty-nest/src/app.service.ts
+++ chatty-nest/src/app.service.ts
@@ -26,7 +26,7 @@ export class AppService {
 // });
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/parallel1');
+mongoose.connect('mongodb://172.19.0.2:27017/parallel1');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
